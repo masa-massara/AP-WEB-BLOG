8490400a6f259573d882e7c53512f71e633a6e6a
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-01-11 20:11:07 +0900

    投稿後にリロードすると同じ投稿がなされてしまう不具合を修正

--- workspace/blog/post.php
+++ workspace/blog/post.php
@@ -1,3 +1,46 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // タイトルと本文の入力をチェック
+    if (empty($_POST["title"]) || empty($_POST["contents"])) {
+        $title_value = $_POST['title'];
+        $contents_value = $_POST['contents'];
+        $test_alert = "<script type='text/javascript'>alert('タイトルと本文は必須です。');</script>";
+        echo $test_alert;
+    } elseif (!isset($_POST["password"]) || $_POST["password"] != 'correctPass') {
+        $title_value = $_POST['title'];
+        $contents_value = $_POST['contents'];
+        $test_alert = "<script type='text/javascript'>alert('パスワードが違います。');</script>";
+        echo $test_alert;
+    } else {
+        try {
+
+            ini_set("date.timezone", "Asia/Tokyo");
+            //$timeへ成形した年月日および時刻データを格納
+            $time = date("Y.m.d-H:i");
+            //PDOクラスのオブジェクトの作成（blog.dbに接続）
+            $dbh = new PDO('sqlite:blog.db', '', '');
+            //実行するSQL文を$sqlに格納
+            $sql = 'insert into posts (title,contents,date) values (?, ?, ?)';
+            //prepareメソッドでSQL文の準備
+            $sth = $dbh->prepare($sql);
+            //prepareした$sthを実行SQL文の？部に格納する変数を指定
+            $sth->execute(array($_POST["title"], $_POST["contents"], $time));
+
+            $_POST['title'] = null;
+            $_POST["contents"] = null;
+
+            $test_alert = "<script type='text/javascript'>alert('記事を投稿しました！');</script>";
+
+            echo $test_alert;
+
+
+        } catch (PDOException $e) {
+
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,10 +60,10 @@
     ?>" method="post">
         <dl>
             <dt>表題：</dt>
-            <dd><input type="text" name="title" size="60" /></dd>
+            <dd><input type="text" name="title" size="60" value="<?php echo $title_value; ?>" /></dd>
             <dt>本文：</dt>
             <dd>
-                <textarea name="contents" rows="10" cols="60"></textarea>
+                <textarea name=" contents" rows="10" cols="60"><?php echo $contents_value; ?></textarea>
             </dd>
             <dt>パスワード：</dt>
             <dd>
@@ -33,43 +76,6 @@
 
     </form>
 
-
-
-    <!-- タイトルと本文があったら入る -->
-    <?php
-
-    // タイトルと本文の入力をチェック
-    if (empty($_POST["title"]) || empty($_POST["contents"])) {
-        $test_alert = "<script type='text/javascript'>alert('タイトルと本文は必須です。');</script>";
-        echo $test_alert;
-    } elseif (!isset($_POST["password"]) || $_POST["password"] != 'correctPass') {
-        $test_alert = "<script type='text/javascript'>alert('パスワードが違います。');</script>";
-        echo $test_alert;
-    } else {
-        try {
-
-            ini_set("date.timezone", "Asia/Tokyo");
-            //$timeへ成形した年月日および時刻データを格納
-            $time = date("Y.m.d-H:i");
-            //PDOクラスのオブジェクトの作成（blog.dbに接続）
-            $dbh = new PDO('sqlite:blog.db', '', '');
-            //実行するSQL文を$sqlに格納
-            $sql = 'insert into posts (title,contents,date) values (?, ?, ?)';
-            //prepareメソッドでSQL文の準備
-            $sth = $dbh->prepare($sql);
-            //prepareした$sthを実行SQL文の？部に格納する変数を指定
-            $sth->execute(array($_POST["title"], $_POST["contents"], $time));
-
-
-            $test_alert = "<script type='text/javascript'>alert('記事を投稿しました！');</script>";
-            echo $test_alert;
-            
-            exit();
-        } catch (PDOException $e) {
-
-        }
-    }
-    ?>
 </body>
 
 
