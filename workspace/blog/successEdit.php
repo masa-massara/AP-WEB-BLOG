988dc5b66561be951f14f23951055ec3b3393dc5
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-01-12 16:25:50 +0900

    編集後のメッセージを作成

---
+++ workspace/blog/successEdit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>編集成功メッセージ</title>
+</head>
+<script>alert("編集しました");
+    location.href = "./index.php"</script>
+
+<body>
