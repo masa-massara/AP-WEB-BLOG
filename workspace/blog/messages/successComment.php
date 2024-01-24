1d342cc63b35e84e7d496557d89911b048f7b5d5
masa-massara <masaya.yoshikawa.31@icloud.com>
2024-01-25 01:14:08 +0900

    返信の成功メッセージを作成

---
+++ workspace/blog/messages/successComment.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>返信成功メッセージ</title>
+</head>
+<script>alert("返信を投稿しました");
+    location.href = "../index.php"</script>
+
+<body>
+Ï
